a6297504931a777c8398788f68b5a8a91c0c3957
houssain <houssain_khalifa@hotmail.com>
2018-03-15 23:47:02 +0100

    push de la version nettoyé

---
+++ admin/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * POUR CONNEXION DB
+ */
+
+define("DB_HOST", "sqlgold.phpnet.org:48614");
+define("DB_LOGIN", "sql8614_10");
+define("DB_PWD", "dypt0TRkKiqL");
+define("DB_NAME", "sql8614_10");
+define("DB_CHARSET", "utf8");
+$par_page = 3;
+$lulu="pitiag";
